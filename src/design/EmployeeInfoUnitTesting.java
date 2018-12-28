8e7932f8919826e989fb1494200382fe693f593d
Zaya0619 <42502056+Zaya0619@users.noreply.github.com>
2018-12-28 17:54:28 -0500

    Update EmployeeInfoUnitTesting.java

--- src/design/EmployeeInfoUnitTesting.java
+++ src/design/EmployeeInfoUnitTesting.java
@@ -9,6 +9,13 @@ public class EmployeeInfoUnitTesting {
         //Write Unit Test for all the methods has been implemented in this package.
 
         EmployeeInfo employee1 = new EmployeeInfo("John", 1212);
+     
+        int result1 = cal.salary(8000, 12);
+        Assert.assertEquals(96000);
+        int result2 = cal.bonus(8000, .1, 12);
+        Assert.assertEquals(9600);
+        int result3 = cal.pension(8000, .25);
+        Assert.assertEquals(2000);
 
         //Assert.assertEquals(EmployeeInfo.calculateEmployeeSalary());
         //Assert.assertEquals(EmployeeInfo.calculateEmployeeBonus((8000, 8), 9600);
